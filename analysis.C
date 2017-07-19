e05afe53e2ae6656a1fbbeabbe61c096d5d3bba2
Katherine Melbourne <melbourne@cuore-login2.lngs.infn.it>
2017-07-19 22:34:52 +0200

    Added summary. Data set used now up-to-date.

--- analysis.C
+++ analysis.C
@@ -1,20 +1,9 @@
-/* This script uses CUORE calibration data to create histograms of different peak energy events within each Tellurium crystal.
-
-To run:
->root
->.L analysis.cpp
->plot_AllString_Th232Peaks(Time)
-
-where "Time" is the total time from the simulation (can be grabbed from the Time histogram from the g4cuore output file)
+/* This script plots all real CUORE calibration histograms on one canvas with a different color for each peak.
 
 Written by: Katie Melbourne
 katherine.melbourne@yale.edu
 */
 
-#ifndef __CINT__
-#endif
-
-#include "TROOT.h"
 #include "TTree.h"
 #include "TSystem.h"
 #include "TFile.h"
@@ -22,15 +11,12 @@ katherine.melbourne@yale.edu
 #include "TH1F.h"
 #include "TH1D.h"
 #include "TGraph.h"
-#include "TGraphAsymmErrors.h"
 #include "TStyle.h"
 #include "TCanvas.h"
 #include "TLegend.h"
 #include "TCut.h"
-#include <iostream>
 #include "THStack.h"
 #include "TMath.h"
-
 #include "TAxis.h"
 
 void plot_peaks()
@@ -40,7 +26,7 @@ void plot_peaks()
   int xmax = 988;
   
   QChain * ch = new QChain("qtree");
-  ch->Add("/nfs/cuore1/scratch/shared_output/output/ds3015/calibration_Production_ds3015.list");
+  ch->Add("/nfs/cuore1/scratch/shared_output/output/ds3018_unblinded/calibration_UnblindedReduced_ds3018.list");
 
  //Defining individual peak histograms to be filled with cut energy
   TH1F* Peak2615 = new TH1F("Peak2615", "Peak2615", nbins, xmin, 988);
@@ -49,35 +35,33 @@ void plot_peaks()
   TH1F* Peak583 = new TH1F("Peak583", "Peak583", nbins, xmin, 988);
   TH1F* Peak338 = new TH1F("Peak338", "Peak338", nbins, xmin, 988);
   TH1F* Peak239 = new TH1F("Peak239", "Peak239", nbins, xmin, 988);
-
-  TCut("Filter_RejectBadIntervals");
-  TCut("IsSignal");
-  TCut("Filter_NumberofPulses==1");
   
   //Applying cuts on Energy to separate each peak
-  TCut cut3 = "Energy > 2605";
-  TCut cut4 = "Energy < 2625";
-  TCut cut2615 = cut3 && cut4;
+  TCut basecut = "QNDBDAnalysisBaseCut";
+  
+  TCut cut3 = "QNDBDOfficialEnergy > 2605";
+  TCut cut4 = "QNDBDOfficialEnergy < 2625";
+  TCut cut2615 = cut3 && cut4 && basecut;
 
-  TCut cut5 = "Energy > 959";
-  TCut cut6 = "Energy < 979";
-  TCut cut969 = cut5 && cut6;
+  TCut cut5 = "QNDBDOfficialEnergy > 959";
+  TCut cut6 = "QNDBDOfficialEnergy < 979";
+  TCut cut969 = cut5 && cut6 && basecut;
 
-  TCut cut7 = "Energy > 901";
-  TCut cut8 = "Energy < 921";
-  TCut cut911 = cut7 && cut8;
+  TCut cut7 = "QNDBDOfficialEnergy > 901";
+  TCut cut8 = "QNDBDOfficialEnergy < 921";
+  TCut cut911 = cut7 && cut8 && basecut;
 
-  TCut cut9 = "Energy > 573";
-  TCut cut10 = "Energy < 593";
-  TCut cut583 = cut9 && cut10;
+  TCut cut9 = "QNDBDOfficialEnergy > 573";
+  TCut cut10 = "QNDBDOfficialEnergy < 593";
+  TCut cut583 = cut9 && cut10 && basecut;
 
-  TCut cut11 = "Energy > 323";
-  TCut cut12 = "Energy < 353";
-  TCut cut338 = cut11 && cut12;
+  TCut cut11 = "QNDBDOfficialEnergy > 323";
+  TCut cut12 = "QNDBDOfficialEnergy < 353";
+  TCut cut338 = cut11 && cut12 && basecut;
 
-  TCut cut13 = "Energy > 229";
-  TCut cut14 = "Energy < 249";
-  TCut cut239 = cut13 && cut14;
+  TCut cut13 = "QNDBDOfficialEnergy > 229";
+  TCut cut14 = "QNDBDOfficialEnergy < 249";
+  TCut cut239 = cut13 && cut14 && basecut;
 
   TCanvas* c3 = new TCanvas("c3", "c3", 600, 600);
 
@@ -101,7 +85,6 @@ void plot_peaks()
   leg->AddEntry(Peak239,"239 KeV");
 
   //Filling peak histograms with Channels that have been altered by cut energy
-
   ch->Draw("Channel >> Peak2615", cut2615);
   ch->Draw("Channel >> Peak969", cut969);
   ch->Draw("Channel >> Peak911", cut911);
